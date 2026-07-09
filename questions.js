6ae529a0e2010883af1d4a45ab05f3ff4a078ab2
damstrivandrum <aajalms@gmail.com>
2026-07-09 12:07:08 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -2,7 +2,7 @@
 window.questions = [
   {
     id: 1,
-    image: "images/IMG_9093.jpeg",
+    image: "images/ScreenRecording_07-09-2026 11-58-33_1.mov",
     question: "A 7-year-old unimmunized child is brought with high-grade fever, cough, coryza, conjunctival congestion, and marked photophobia. On oral examination, multiple tiny bluish-white lesions surrounded by an erythematous halo are seen on the buccal mucosa opposite the molar teeth. Two days later, a maculopapular rash appears behind the ears and spreads downward.\nWhich of the following is the most likely diagnosis?",
     options: [
       "Measles",
